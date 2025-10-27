90931f831b1b37efedbcb2eab08eb583c087b036
KEA228 <ykashpanov.e@bk.ru>
2025-10-27 20:56:53 +0300

    Update our.cpp

--- our.cpp
+++ our.cpp
@@ -1,5 +1,15 @@
-//#include "graphics.h"
-//#include "picture.hpp"
-//void fence () {
+#include "graphics.h"
+#include "picture.hpp"
+void fence () 
+{
+   setcolor(BLUE);
+   rectangle(200, 500, 230, 400);
+    rectangle(235, 500, 265, 400);
+    rectangle(270, 500, 300, 400);
+    rectangle(305, 500, 335, 400);
+    rectangle(340,500, 370, 400);
+    rectangle(375, 500, 405, 400);
+   
 
-//}
+  
+}
